8a4c6b043a36e8778801da943b29be951c4d2b02
Naufal Fadhlurrohman <naufal.fadhlurrohman@agilis.id>
2026-06-06 20:54:23 +0700

    hehe

--- script.js
+++ script.js
@@ -876,3 +876,18 @@ function initPhysicsIDCard() {
 document.addEventListener('DOMContentLoaded', () => {
     initPhysicsIDCard();
 });
+
+// Blokir Klik Kanan
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// Blokir Shortcuts Inspect Element
+document.addEventListener('keydown', (e) => {
+    if (
+        e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C')) ||
+        (e.ctrlKey && e.key === 'U')
+    ) {
+        e.preventDefault();
+        return false;
+    }
+});
